ceffb5b9d151bb7ba24cfea28bb2643bab34db72
Unknown <shobu13@hotmail.fr>
2018-05-02 01:05:15 +0200

    récupération des membres du projet

--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from core.models import Projet, Membre
+
 
 def core_home(request):
     """vue affichant la page d'acceuil, renvoie le queryset du projet phare"""
-    return render(request, 'core/coreHome.html')
+    projet_phare = Projet.objects.get(phare=True)
+    chef = projet_phare.chef
+    graphistes = projet_phare.graphiste.all()
+    musiciens = projet_phare.musicien.all()
+    codeurs = projet_phare.codeur.all()
+    return render(request, 'core/coreHome.html', locals())
