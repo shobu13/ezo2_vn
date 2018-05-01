6ee14e984d31043229328fc334c70de008fac34a
Unknown <shobu13@hotmail.fr>
2018-05-01 23:53:59 +0200

    ajout système création projet/membres

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from core.models import Projet, Membre
+
+
+class ProjetAdmin(admin.ModelAdmin):
+
+    list_display = ('nom', 'chef', 'etat')
+    list_filter = ('chef', 'etat')
+    search_fields = ('nom', )
+    ordering = ('nom', )
+    prepopulated_fields = {'slug': ('nom',), }
+
+
+admin.site.register(Projet, ProjetAdmin)
+admin.site.register(Membre)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from markdownx.models import MarkdownxField
+
+
+class Projet(models.Model):
+    """class définissant chaque projet en cours, une description du projet,
+    son header et son titre"""
+    nom = models.CharField(max_length=100)
+    slug = models.SlugField()
+    description = MarkdownxField()
+    header = models.ImageField(upload_to='projet/img/upload/')
+    chef = models.OneToOneField('Membre', verbose_name="chef de projet",
+                                  related_name="chef",null=True, blank=True, on_delete=models.PROTECT)
+    graphiste = models.ManyToManyField('Membre', verbose_name="Graphiste(s)",
+                                       related_name="graphiste", blank=True)
+    musicien = models.ManyToManyField('Membre', verbose_name="Musicien(s)",
+                                      related_name="musicien", blank=True)
+    codeur = models.ManyToManyField('Membre', verbose_name="Codeur(s)",
+                                    related_name="codeur", blank=True)
+    etat = models.BooleanField(verbose_name="Terminé ?", default=False)
+
+    def __str__(self):
+        return self.nom
+
+
+class Membre(models.Model):
+    """class définissant les membres producteur de la communautée, rattaché ou non à
+    des projets."""
+    pseudo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.pseudo
