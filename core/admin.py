1f583a778713e620895705e5a3050ecdbc07ca88
Unknown <shobu13@hotmail.fr>
2018-05-02 18:59:09 +0200

    champ chef de projet obligatoir+ajout custom tabs

--- core/admin.py
+++ core/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
 
-
 from core.models import Projet, Membre
 
 
 class ProjetAdmin(admin.ModelAdmin):
-
-    list_display = ('nom', 'chef', 'etat')
-    list_filter = ('chef', 'etat', 'phare')
-    search_fields = ('nom', )
-    ordering = ('nom', )
+    list_display = ('nom', 'etat')
+    list_filter = ('etat', 'phare')
+    search_fields = ('nom',)
+    ordering = ('nom',)
     prepopulated_fields = {'slug': ('nom',), }
+    fieldsets = (
+        ('Informations Générales', {
+            # définis les champs qui y sont représenter
+            'fields': ('nom', 'slug', 'description', 'header')
+        }),
+        ('Membres', {
+            # définis les champs qui y sont représenter
+            'fields': ('chef', 'graphiste', 'musicien', 'scenariste', 'codeur')
+        }),
+        ('Customisation tabs', {
+            'description': '<a href="http://materializecss.com/color.html#palette"  onclick="window.open(this.href); return false;">palette de couleur</a>',
+            'classes': ('collapse',),
+            'fields': (
+                'couleur_tab_texte', 'couleur_tab_active_background', 'couleur_tab_soulignement')
+        }),
+        ('Autre', {
+            # définis les champs qui y sont représenter
+            'fields': ('etat', 'phare',)
+        }),
+
+    )
 
 
 admin.site.register(Projet, ProjetAdmin)
--- core/models.py
+++ core/models.py
@@ -11,18 +11,23 @@ class Projet(models.Model):
     slug = models.SlugField()
     description = MarkdownxField()
     header = models.ImageField(upload_to='projet/img/upload/')
-    chef = models.OneToOneField('Membre', verbose_name="chef de projet",
-                                  related_name="chef",null=True, blank=True, on_delete=models.PROTECT)
+    chef = models.ForeignKey('Membre', on_delete=models.PROTECT, null=True,
+                             verbose_name="chef de projet")
     graphiste = models.ManyToManyField('Membre', verbose_name="Graphiste(s)",
                                        related_name="graphiste", blank=True)
     musicien = models.ManyToManyField('Membre', verbose_name="Musicien(s)",
                                       related_name="musicien", blank=True)
     scenariste = models.ManyToManyField('Membre', verbose_name="Scénariste(s)",
-                                      related_name="scenariste", blank=True)
+                                        related_name="scenariste", blank=True)
     codeur = models.ManyToManyField('Membre', verbose_name="Codeur(s)",
                                     related_name="codeur", blank=True)
     etat = models.BooleanField(verbose_name="Terminé ?", default=False)
     phare = models.BooleanField(verbose_name="Projet Phare ?", default=False)
+
+    # tab customisation
+    couleur_tab_texte = models.CharField(max_length=7, default="#d500f9", blank=True)
+    couleur_tab_active_background = models.CharField(max_length=7, default="#f3e5f5", blank=True)
+    couleur_tab_soulignement = models.CharField(max_length=7, default="#4a148c", blank=True)
 
     def __str__(self):
         return self.nom
