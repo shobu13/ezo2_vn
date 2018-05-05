4759a0b067e76f7a1c72e8fdf5d48af321b9011c
Unknown <shobu13@hotmail.fr>
2018-05-05 22:03:50 +0200

    prod setting

--- ezo2_vn/settings.py
+++ ezo2_vn/settings.py
@@ -129,5 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join("/var/www/ezo2_vn", "media")
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join("/var/www/ezo2_vn", "static")
