7ee976a6163e030fead7e1b761fa01cd4e3da522
Unknown <shobu13@hotmail.fr>
2018-05-05 22:23:20 +0200

    dev setting

--- ezo2_vn/settings.py
+++ ezo2_vn/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'ezo2_vn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ezo2_vn',
+        'USER': 'ezo-chan',
+        'PASSWORD': 'C9@Q5<sh',
+        'HOST': 'localhost',
     }
 }
 
